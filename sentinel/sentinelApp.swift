d9ed0e3a9a749ce7cc01929d464965826523cd93
Kostandin Angjellari <kostandinangjellari@gmail.com>
2025-11-02 17:19:04 +0100

    Initial Commit

---
+++ sentinel/sentinelApp.swift
@@ -0,0 +1,17 @@
+//
+//  sentinelApp.swift
+//  sentinel
+//
+//  Created by Kostandin Angjellari on 2.11.25.
+//
+
+import SwiftUI
+
+@main
+struct sentinelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
